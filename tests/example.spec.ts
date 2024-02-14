e8782e4e986ded129089b28bdf64e59bdfd9a807
Neil Kalman <neil.kalman@growthspace.com>
2024-02-14 12:17:18 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,13 +4,13 @@ const describe = test.describe;
 
 describe('application title', () => {
   test('has correct title', async ({ page }) => {
-    await page.goto('https://thatkookooguy.github.io/arcade-playlist/game-collection?sortBy=A%20-%20Z');
+    await page.goto('https://arcade.kibibit.duckdns.org/game-collection?sortBy=A%20-%20Z');
   
     await expect(page).toHaveTitle(/Arcade Playlist/);
   });
   
   test('game playlist header cover', async ({ page }) => {
-    await page.goto('https://thatkookooguy.github.io/arcade-playlist/game-collection?sortBy=A%20-%20Z');
+    await page.goto('https://arcade.kibibit.duckdns.org/game-collection?sortBy=A%20-%20Z');
   
     await expect(page.locator('.kb-playlist-image')).toBeVisible();
   });
